342149b026f9e03870d23a548806fc36145ad0a5
Hugo Leonel <hugoleonel.code@gmail.com>
2023-06-05 23:53:47 -0300

    Exemplos Querys repositorio Task

--- src/main/java/com/hugoleonel/todosimple/repositories/TaskRepository.java
+++ src/main/java/com/hugoleonel/todosimple/repositories/TaskRepository.java
@@ -8,4 +8,11 @@ import com.hugoleonel.todosimple.models.Task;
 @Repository
 public interface TaskRepository extends JpaRepository<Task,Long>{
     
+    // JPQL
+    //@Query(value = "SELECT t FROM Task t WHERE t.user.id = :id")
+
+    //SQL
+   // @Query(value = "SELECT * FROM task t WHERE t.user_id = :id", nativeQuery = true)
+   // List<Task> findByUser_Id(@Param("id") Long id);
+
 }
